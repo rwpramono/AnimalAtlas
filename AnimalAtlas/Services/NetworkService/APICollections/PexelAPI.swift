13f090626c0f3ef43052969638d1e484c569409c
Rachmat Wahyu Pramono <rwpramono@gmail.com>
2024-07-28 07:25:06 +0700

    rename PexelAPI

--- AnimalAtlas/Services/NetworkService/APICollections/PexelAPI.swift
+++ AnimalAtlas/Services/NetworkService/APICollections/PexelAPI.swift
@@ -1,5 +1,5 @@
 //
-//  PaxelAPI.swift
+//  PexelAPI.swift
 //  AnimalAtlas
 //
 //  Created by Rachmat Wahyu Pramono on 25/07/24.
@@ -7,12 +7,12 @@
 
 import Foundation
 
-public struct PaxelAPI<T: Decodable>: APIDataRequest {
+public struct PexelAPI<T: Decodable>: APIDataRequest {
     public private(set) var urlRequest: URLRequest?
     public private(set) var urlComponent: URLComponents?
     
     init(_ httpMethod: HttpMethod, path: String) {
-        guard let urlComponent = URLComponents(string: "some base url" + path),
+        guard let urlComponent = URLComponents(string: "https://api.pexels.com/v1/" + path),
               let completeUrl = urlComponent.url else {
             self.urlRequest = nil
             self.urlComponent = nil
@@ -22,24 +22,25 @@ public struct PaxelAPI<T: Decodable>: APIDataRequest {
         urlRequest.httpMethod = httpMethod.rawValue
         urlRequest.allHTTPHeaderFields = [
             "Content-Type": "application/json",
-            "Authorization": "some api key"
+            "Authorization": "F0RsC7L6viQO7bzFmZTKs7hwGWhXlwm5TjAozyXUwkTmB8INisxbwjVg"
         ]
         self.urlComponent = urlComponent
         self.urlRequest = urlRequest
     }
 
-    public func body(_ body: [String : Any]) -> PaxelAPI<T> {
+    public func body(_ body: [String : Any]) -> PexelAPI<T> {
         var apiAttribute = self
         let bodyData = try? JSONSerialization.data(withJSONObject: body)
         apiAttribute.urlRequest?.httpBody = bodyData
         return apiAttribute
     }
     
-    public func query(_ query: [String : String]) -> PaxelAPI<T> {
+    public func query(_ query: [String : String]) -> PexelAPI<T> {
         var apiAttribute = self
         apiAttribute.urlComponent?.queryItems = query.map {
             URLQueryItem(name: $0.key, value: $0.value)
         }
+        apiAttribute.urlRequest?.url = apiAttribute.urlComponent?.url
         return apiAttribute
     }
 }
