78f5ae25a086acc5cfa5a3b883f17674c76480c7
Rachmat Wahyu Pramono <rwpramono@gmail.com>
2024-07-28 07:26:31 +0700

    refine AnimalResponse due to json value not consistent

--- AnimalAtlas/Models/AnimalResponse.swift
+++ AnimalAtlas/Models/AnimalResponse.swift
@@ -12,11 +12,11 @@ public typealias AnimalResponse = [AnimalResponseElement]
 // MARK: - AnimalResponseElement
 public struct AnimalResponseElement: Codable {
     public let name: String
-    public let taxonomy: Taxonomy
-    public let locations: [String]
-    public let characteristics: Characteristics
+    public let taxonomy: AnimalAnimalLocation
+    public let locations: [Location]
+    public let characteristics: AnimalCharacteristics
 
-    public init(name: String, taxonomy: Taxonomy, locations: [String], characteristics: Characteristics) {
+    public init(name: String, taxonomy: AnimalAnimalLocation, locations: [Location], characteristics: AnimalCharacteristics) {
         self.name = name
         self.taxonomy = taxonomy
         self.locations = locations
@@ -24,15 +24,25 @@ public struct AnimalResponseElement: Codable {
     }
 }
 
-// MARK: - Characteristics
-public struct Characteristics: Codable {
-    public let prey, nameOfYoung, groupBehavior, estimatedPopulationSize: String
-    public let biggestThreat, mostDistinctiveFeature, otherNameS, gestationPeriod: String
-    public let habitat, predators, diet, averageLitterSize: String
-    public let lifestyle, commonName, numberOfSpecies, location: String
-    public let slogan, group, color, skinType: String
-    public let topSpeed, lifespan, weight, height: String
-    public let ageOfSexualMaturity, ageOfWeaning: String
+// MARK: - AnimalCharacteristics
+public struct AnimalCharacteristics: Codable {
+    public let prey, nameOfYoung: String?
+    public let groupBehavior: GroupBehavior?
+    public let estimatedPopulationSize, biggestThreat, mostDistinctiveFeature, otherNameS: String?
+    public let gestationPeriod: String?
+    public let habitat: String
+    public let predators: String?
+    public let diet: Diet
+    public let averageLitterSize: String
+    public let lifestyle: GroupBehavior
+    public let commonName, numberOfSpecies, location: String?
+    public let slogan: String
+    public let group: String?
+    public let color: String
+    public let skinType: SkinType
+    public let topSpeed, lifespan, weight: String
+    public let height, ageOfSexualMaturity, ageOfWeaning, mainPrey: String?
+    public let distinctiveFeature, favoriteFood, type, length: String?
 
     public enum CodingKeys: String, CodingKey {
         case prey
@@ -54,9 +64,13 @@ public struct Characteristics: Codable {
         case lifespan, weight, height
         case ageOfSexualMaturity = "age_of_sexual_maturity"
         case ageOfWeaning = "age_of_weaning"
+        case mainPrey = "main_prey"
+        case distinctiveFeature = "distinctive_feature"
+        case favoriteFood = "favorite_food"
+        case type, length
     }
 
-    public init(prey: String, nameOfYoung: String, groupBehavior: String, estimatedPopulationSize: String, biggestThreat: String, mostDistinctiveFeature: String, otherNameS: String, gestationPeriod: String, habitat: String, predators: String, diet: String, averageLitterSize: String, lifestyle: String, commonName: String, numberOfSpecies: String, location: String, slogan: String, group: String, color: String, skinType: String, topSpeed: String, lifespan: String, weight: String, height: String, ageOfSexualMaturity: String, ageOfWeaning: String) {
+    public init(prey: String?, nameOfYoung: String?, groupBehavior: GroupBehavior?, estimatedPopulationSize: String?, biggestThreat: String?, mostDistinctiveFeature: String?, otherNameS: String?, gestationPeriod: String?, habitat: String, predators: String?, diet: Diet, averageLitterSize: String, lifestyle: GroupBehavior, commonName: String?, numberOfSpecies: String?, location: String?, slogan: String, group: String?, color: String, skinType: SkinType, topSpeed: String, lifespan: String, weight: String, height: String?, ageOfSexualMaturity: String?, ageOfWeaning: String?, mainPrey: String?, distinctiveFeature: String?, favoriteFood: String?, type: String?, length: String?) {
         self.prey = prey
         self.nameOfYoung = nameOfYoung
         self.groupBehavior = groupBehavior
@@ -83,28 +97,84 @@ public struct Characteristics: Codable {
         self.height = height
         self.ageOfSexualMaturity = ageOfSexualMaturity
         self.ageOfWeaning = ageOfWeaning
+        self.mainPrey = mainPrey
+        self.distinctiveFeature = distinctiveFeature
+        self.favoriteFood = favoriteFood
+        self.type = type
+        self.length = length
     }
 }
 
-// MARK: - Taxonomy
-public struct Taxonomy: Codable {
-    public let kingdom, phylum, taxonomyClass, order: String
-    public let family, genus, scientificName: String
+public enum Diet: String, Codable {
+    case carnivore = "Carnivore"
+    case herbivore = "Herbivore"
+    case omnivore = "Omnivore"
+}
+
+public enum GroupBehavior: String, Codable {
+    case diurnal = "Diurnal"
+    case herd = "Herd"
+    case solitary = "Solitary"
+}
+
+public enum SkinType: String, Codable {
+    case fur = "Fur"
+    case leather = "Leather"
+}
+
+public enum Location: String, Codable {
+    case africa = "Africa"
+    case asia = "Asia"
+    case ocean = "Ocean"
+}
+
+// MARK: - AnimalAnimalLocation
+public struct AnimalAnimalLocation: Codable {
+    public let kingdom: AnimalKingdom
+    public let phylum: AnimalPhylum
+    public let taxonomyAnimalClass: AnimalClass
+    public let order: AnimalOrder
+    public let family: AnimalFamily
+    public let genus, scientificName: String?
 
     public enum CodingKeys: String, CodingKey {
         case kingdom, phylum
-        case taxonomyClass = "class"
+        case taxonomyAnimalClass = "class"
         case order, family, genus
         case scientificName = "scientific_name"
     }
 
-    public init(kingdom: String, phylum: String, taxonomyClass: String, order: String, family: String, genus: String, scientificName: String) {
+    public init(kingdom: AnimalKingdom, phylum: AnimalPhylum, taxonomyAnimalClass: AnimalClass, order: AnimalOrder, family: AnimalFamily, genus: String?, scientificName: String?) {
         self.kingdom = kingdom
         self.phylum = phylum
-        self.taxonomyClass = taxonomyClass
+        self.taxonomyAnimalClass = taxonomyAnimalClass
         self.order = order
         self.family = family
         self.genus = genus
         self.scientificName = scientificName
     }
+}
+
+public enum AnimalFamily: String, Codable {
+    case elephantidae = "Elephantidae"
+    case macroscelididae = "Macroscelididae"
+    case phocidae = "Phocidae"
+}
+
+public enum AnimalKingdom: String, Codable {
+    case animalia = "Animalia"
+}
+
+public enum AnimalOrder: String, Codable {
+    case carnivora = "Carnivora"
+    case macroscelidea = "Macroscelidea"
+    case proboscidea = "Proboscidea"
+}
+
+public enum AnimalPhylum: String, Codable {
+    case chordata = "Chordata"
+}
+
+public enum AnimalClass: String, Codable {
+    case mammalia = "Mammalia"
 }
